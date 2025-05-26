b8efdd7f7c1ec3d030635cea8a73681ad4acfdf4
jamdarnayan <nayanjamdar1@gmail.com>
2025-05-27 01:42:58 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ✅ ensure paths are correct after build
+  base: '/Healthcare-Dashboard/',
   plugins: [react()],
 })
