8de091bcb6c156dc1f047e16e46e283d2d537b89
jamdarnayan <nayanjamdar1@gmail.com>
2025-05-27 01:23:29 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../src/styles/app.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
